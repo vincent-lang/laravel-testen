65479ef69b5a9353e30c0a4c8e4c91ab01bc84c5
vincent van de merwe <335479@student.mboutrecht.nl>
2024-04-15 11:29:17 +0200

    added a test on database data in a table if it says empty or not

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div class="table">
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Prijs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($products as $product)
+                    <tr>
+                        <td>{{$product->name}}</td>
+                        <td>{{$product->price->price}}</td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="2">{{__('Er zijn geen producten.')}}</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Price;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_homepage_contains_empty_table(): void
+    {
+        $response = $this->get('/products');
+
+        $response->assertStatus(200);
+        $response->assertSee(__(key: 'Er zijn geen producten.'));
+    }
+
+    public function test_homepage_contains_non_empty_table(): void
+    {
+        Product::create([
+            'name' => 'melk',
+            'price_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        Price::create([
+            'price' => '1.20',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $response = $this->get('/products');
+
+        $response->assertStatus(200);
+        $response->assertDontSee(__(key: 'Er zijn geen producten.'));
+    }
+}
